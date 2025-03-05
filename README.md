8ed4517bf50686d7fb2fd8941705ee1aaf7fc7f1
Julie Poignant <julie.cpoignant@gmail.com>
2025-03-05 11:32:33 +0100

    Image optimization
    Updated multer-config.js so the uploaded images get changed to webp format and compressed.
    
    Multer now register the file with without keeping the original extension in the original file name.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Mon_Vieux_Grimoire
  
+## START PROJECT
+
+Dans un premier terminal, tapez 'cd frontend' puis 'npm install' et enfin 'npm start'.
+Dans un second terminal, tapez 'cd backend' puis 'npm install' et enfin 'nodemon server'.
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -4,21 +4,15 @@ const path = require('path');
 const fs = require('fs');
 
 
-const MIME_TYPES = {
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpg',
-    'image/png': 'png'
-};
-
 // TELLING TO MULTER WHERE TO STORE THE FILES AND HOW TO CREATE THEIR NAME
 const storage = multer.diskStorage({
     destination: (req, file, callback) => {
         callback(null, 'images');
     },
     filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
-        const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.' + extension);
+        const filename = path.parse(file.originalname).name
+        const name = filename.split(' ').join('_');
+        callback(null, name + Date.now() + '.webp');
     }
 });
 
@@ -32,18 +26,22 @@ module.exports.resizeImage = (req, res, next) => {
     }
 
     const filePath = req.file.path;
-    const outputFilePath = path.join('images', `resized_${req.file.filename}`);
+    const filename = path.parse(req.file.filename).name
+    const outputFilePath = path.join('images', `resized_${filename}.webp`);
 
     sharp(filePath)
-        .resize({ width: 206, height: 260 })
+        // We change the file format to webp
+        .toFormat('webp')
         .toFile(outputFilePath)
         .then(() => {
-            // We replace the previous file with the resized one
+
+            // We replace the previous file with the resized one in the API data
             fs.unlink(filePath, () => {
                 req.file.path = outputFilePath;
                 next();
             });
         })
+
         .catch(err => {
             console.log(err);
             return next();
